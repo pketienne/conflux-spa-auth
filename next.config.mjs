d0bc44c61d0040c6f4dd8f5888e1d447c4300fdf
Patrick K. Etienne <git@paradox.limited>
2024-03-27 21:19:08 -0400

    Updating / upgrading configuration files (in prep for porting over PrimeReact theme).

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/apps/mail',
+        destination: '/apps/mail/inbox',
+        permanent: true
+      }
+    ];
+  }
+};
 
 export default nextConfig;
